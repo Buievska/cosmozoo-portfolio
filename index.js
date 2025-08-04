d2abb0b85ef8d213b8b72dc157f5be852bcafde4
Buievska <anna111239@gmail.com>
2025-08-04 19:46:36 +0000

    Deploying to gh-pages from @ Buievska/cosmozoo-portfolio@389e38038a4972777343ece768b9236553954174 🚀

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import"./assets/main-VHzxEShR.js";import{s as l}from"./assets/data-BI-xCbn2.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector(".portfolio-home-list");if(!e)return;const i=["tea","sand"];l.filter(t=>i.includes(t.id)).forEach((t,s)=>{const o=document.createElement("li"),n=s===0?"portfolio-home-item-one":"portfolio-home-item-two";o.classList.add(n),o.innerHTML=`
+import"./assets/main-VHzxEShR.js";import{s as l}from"./assets/data-CXVA8XY_.js";document.addEventListener("DOMContentLoaded",()=>{const e=document.querySelector(".portfolio-home-list");if(!e)return;const i=["tea","sand"];l.filter(t=>i.includes(t.id)).forEach((t,s)=>{const o=document.createElement("li"),n=s===0?"portfolio-home-item-one":"portfolio-home-item-two";o.classList.add(n),o.innerHTML=`
       <a href="shoot.html?id=${t.id}" class="portfolio-home-btn">
         <p class="portfolio-home-title-btn">${t.title}</p>
         <p class="portfolio-home-text-btn">View details</p>
